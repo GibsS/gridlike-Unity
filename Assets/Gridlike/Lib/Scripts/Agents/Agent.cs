23f89b7803281f3c585d0284b4ebe9232c4fe999
gibson <emerick.gibson@hotmail.fr>
2017-10-15 12:18:09 +0100

    Added agents

---
+++ Assets/Gridlike/Lib/Scripts/Agents/Agent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour {
+
+	public static List<Agent> agents { get; private set; }
+
+	void Start() {
+		if (agents != null) agents = new List<Agent> ();
+
+		agents.Add (this);
+	}
+}
