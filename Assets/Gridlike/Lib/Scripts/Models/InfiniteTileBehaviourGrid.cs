f58e73f3e8c024af26a55e866c75b1825d6eeb79
gibson <emerick.gibson@hotmail.fr>
2017-10-21 18:43:09 +0100

    WIP on tile GO

---
+++ Assets/Gridlike/Lib/Scripts/Models/InfiniteTileBehaviourGrid.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class InfiniteTileBehaviourGrid {
+
+	[SerializeField] InfiniteComponentGrid componentGrid;
+	[SerializeField] GameObject container;
+
+	public InfiniteTileBehaviourGrid(GameObject parent, int regionSize) {
+		componentGrid = new InfiniteComponentGrid (regionSize);
+
+		foreach (Transform t in parent.transform) {
+			if (t.name == "tile container") {
+				UnityEngine.Object.DestroyImmediate (t.gameObject);
+			}
+		}
+
+		if (container == null) {
+			container = new GameObject ("tile container");
+			container.transform.SetParent (parent.transform, false);
+		}
+	}
+
+	public bool HasTileGO(int x, int y) {
+		return componentGrid.Get (x, y) != null;
+	}
+	public TileBehaviour GetTileBehaviour(int x, int y) {
+		Component component = componentGrid.Get (x, y);
+
+		if (component is TileBehaviour) {
+			return component as TileBehaviour;
+		} else {
+			return null;
+		}
+	}
+	public GameObject GetTileGO(int x, int y) {
+		Component component = componentGrid.Get (x, y);
+
+		if (component != null) {
+			return component.gameObject;
+		} else {
+			return null;
+		}
+	}
+
+	public void CreateTileGO(TileInfo info, int x, int y) {
+		if (info.tileGO != null) {
+			GameObject tile = Object.Instantiate (info.tileGO);
+			tile.transform.SetParent (container.transform);
+			tile.transform.localPosition = new Vector2 (x + 0.5f, y + 0.5f);
+
+			TileBehaviour tileBehaviour = tile.GetComponent<TileBehaviour> ();
+
+			if (tileBehaviour != null) {
+				x += tileBehaviour.areaBottomLeftXOffset;
+				y += tileBehaviour.areaBottomLeftYOffset;
+
+				bool[,] area = tileBehaviour.area;
+
+				for (int i = 0; i < area.GetLength (0); i++) {
+					for (int j = 0; j < area.GetLength (1); j++) {
+						if (area [i, j]) {
+							componentGrid.Set (x + i, y + j, tileBehaviour);
+						}
+					}
+				}
+
+				componentGrid.Set (x, y, tileBehaviour);
+			} else {
+				componentGrid.Set (x, y, tile.transform);
+			}
+		}
+	}
+
+	public void DestroyTileGO(int x, int y) {
+		Component component = componentGrid.Get (x, y);
+
+		if (component != null) {
+			if (component is TileBehaviour) {
+				x = (component as TileBehaviour)._x + (component as TileBehaviour).areaBottomLeftXOffset;
+				y = (component as TileBehaviour)._y + (component as TileBehaviour).areaBottomLeftYOffset;
+
+				bool[,] area = (component as TileBehaviour).area;
+
+				for (int i = 0; i < area.GetLength (0); i++) {
+					for (int j = 0; j < area.GetLength (1); j++) {
+						if (area [i, j]) {
+							componentGrid.Set (x + i, y + j, null);
+						}
+					}
+				}
+			}
+
+			componentGrid.Set (x, y, null);
+			Object.DestroyImmediate (component.gameObject);
+		}
+	}
+
+	public FiniteComponentGrid GetRegion(int regionX, int regionY) {
+		return componentGrid.GetRegion (regionX, regionY);
+	}
+}
--- Assets/Gridlike/Lib/Scripts/TileBehaviour/TileBehaviour.cs
+++ Assets/Gridlike/Lib/Scripts/TileBehaviour/TileBehaviour.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public abstract class TileBehaviour : MonoBehaviour {
 
+	public int _x;
+	public int _y;
+
+	public int x { get { return _x; } }
+	public int y { get { return _y; } }
+
 	public abstract bool[,] area { get; }
 	public abstract int areaBottomLeftXOffset { get; }
 	public abstract int areaBottomLeftYOffset { get; }
